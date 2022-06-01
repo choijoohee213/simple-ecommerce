ffd4d4e4d8f57871b87e0dd3076b48c40c570522
choijoohee213 <choijoohee@naver.com>
2022-06-02 03:04:09 +0900

    feat: Mybatis 사용을 위한 @MapperScan 추가

--- backend/src/main/java/com/choijoohee/ecommerce/EcommerceApplication.java
+++ backend/src/main/java/com/choijoohee/ecommerce/EcommerceApplication.java
@@ -1,9 +1,11 @@
 package com.choijoohee.ecommerce;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.choijoohee.ecommerce.**.repository")
 public class EcommerceApplication {
 
 	public static void main(String[] args) {

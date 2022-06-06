17ec7303cc563fb30b1bce02b110c218de973a6f
choijoohee213 <choijoohee@naver.com>
2022-06-07 00:55:10 +0900

    refactor: exception 메세지 처리를 위한 상수 문자열 추가

--- backend/src/main/java/com/choijoohee/ecommerce/product/exception/SoldOutException.java
+++ backend/src/main/java/com/choijoohee/ecommerce/product/exception/SoldOutException.java
@@ -4,14 +4,9 @@ package com.choijoohee.ecommerce.product.exception;
  * 상품의 재고가 없어서 품절 상태에서 장바구니에 넣으려하거나 구매를 하려할 경우 발생하는 예외
  */
 public class SoldOutException extends RuntimeException {
+	private static final String SOLD_OUT_PRODUCT_MESSAGE = "상품의 재고가 없습니다.";
+
 	public SoldOutException() {
-	}
-
-	public SoldOutException(String message) {
-		super(message);
-	}
-
-	public SoldOutException(String message, Throwable cause) {
-		super(message, cause);
+		super(SOLD_OUT_PRODUCT_MESSAGE);
 	}
 }

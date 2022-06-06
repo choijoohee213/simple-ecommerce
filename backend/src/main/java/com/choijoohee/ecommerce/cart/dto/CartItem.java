a8dad53e793d7fdebbd12ad0fef6630ea047a92e
choijoohee213 <choijoohee@naver.com>
2022-06-07 03:22:42 +0900

    refactor: 여러 버젼의 생성자 추가

--- backend/src/main/java/com/choijoohee/ecommerce/cart/dto/CartItem.java
+++ backend/src/main/java/com/choijoohee/ecommerce/cart/dto/CartItem.java
@@ -1,5 +1,7 @@
 package com.choijoohee.ecommerce.cart.dto;
 
+import com.choijoohee.ecommerce.product.dto.Product;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -10,4 +12,18 @@ public class CartItem {
 	private String name;
 	private int price;
 	private int quantity;
+
+	public CartItem(Product product) {
+		this.productId = product.getId();
+		this.name = product.getName();
+		this.price = product.getPrice();
+		this.quantity = 1;
+	}
+
+	public CartItem(int productId, CartItem selectedItem) {
+		this.productId = productId;
+		this.name = selectedItem.getName();
+		this.price = selectedItem.getPrice();
+		this.quantity = selectedItem.getQuantity();
+	}
 }

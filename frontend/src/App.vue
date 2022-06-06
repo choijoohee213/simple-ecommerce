0e584552ee0a7510f95682a43852d090445ad195
choijoohee213 <choijoohee@naver.com>
2022-06-07 01:05:13 +0900

    feat: 메인 페이지 nav 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/product">Product</router-link> |
+      <router-link to="/cart">Cart</router-link>
     </nav>
     <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
 
 <style>
 #app {

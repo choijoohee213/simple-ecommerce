a49ff3ee2f67945445dbafa35735bfac20b0d63c
choijoohee213 <choijoohee@naver.com>
2022-06-07 00:09:36 +0900

    refactor: 공통된 사항들의 클래스를 common 패키지로 이동

--- backend/src/main/java/com/choijoohee/ecommerce/common/config/WebConfig.java
+++ backend/src/main/java/com/choijoohee/ecommerce/common/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.choijoohee.ecommerce.config;
+package com.choijoohee.ecommerce.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
